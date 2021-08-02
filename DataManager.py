9b27fa1066f76dd0a08643751af9b732eb867146
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-08-01 17:02:30 -0700

    renamed main.py to DataManager.py and converted it into a class

---
+++ DataManager.py
@@ -0,0 +1,16 @@
+import Switchboard
+from ListParser import ListParser
+
+class DataManger:
+    def __init__(self):
+        self.user = ''
+        self.pwd = ''
+        self.datatype = ''
+        self.filepath = ''
+        self.payloads = ''
+
+
+
+    # result = Switchboard.send_data(datatype, user, pwd, payloads)
+
+
--- UI/MigratorMainWindow.py
+++ UI/MigratorMainWindow.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
-from PyQt5 import QtGui as qtg
 from UIresources.app_window import Ui_app_window
 
 qtw.QApplication.setAttribute(qtc.Qt.AA_EnableHighDpiScaling, True)  # enables high dpi scaling since I use a 4k monitor
@@ -26,7 +25,6 @@ class AppWindow(qtw.QWidget, Ui_app_window):
     def on_submit(self):
         if self.filepath_field.text() == '' or self.data_type_dropdown.currentIndex() == -1 or self.email_field.text() == '' or self.password_field.text() == '':
             qtw.QMessageBox.warning(self, 'All Fields Required', 'Please make sure you have filled in every field before submitting!.')
-
 
 
 if __name__ == '__main__':
