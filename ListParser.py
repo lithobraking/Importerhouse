c54cf0411851278ad7ef8419803d83e2653914fd
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-08-03 14:55:56 -0700

    ListParser.delete_all_tickets() calls tickets/destroy_many endpoint to delete tickets from Zendesk instance

--- ListParser.py
+++ ListParser.py
@@ -177,4 +177,9 @@ def delete_all_tickets():
         batch_count += 1
         page = []
 
-    print(batches)
+    # the part that does the actual deleting
+    for batch in batches:
+        batch_url = url + batches[batch]
+        print(f'deleting batch {batch}')
+        print(batches[batch])
+        session.delete(batch_url, auth=(Variables.user, Variables.pwd))
