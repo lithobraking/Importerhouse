2ce5c6fb6cde7a6e7df4716e4cf78ec8413a3dfd
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-08-03 14:59:08 -0700

    removed ad hoc function usage

--- UI/DataMigrationTool.py
+++ UI/DataMigrationTool.py
@@ -1,8 +1,6 @@
 import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
-
-import ListParser
 from UIresources.app_window import Ui_app_window
 from DataManager import DataManger
 
@@ -46,7 +44,6 @@ class AppWindow(qtw.QWidget, Ui_app_window):
 
 
 if __name__ == '__main__':
-    ListParser.delete_all_tickets()
     app = qtw.QApplication(sys.argv)
     view = AppWindow()
     view.show()
